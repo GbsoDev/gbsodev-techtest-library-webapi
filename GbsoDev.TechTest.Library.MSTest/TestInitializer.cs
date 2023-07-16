cff11538b311e475f363f264cff48724c8745210
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-15 23:16:26 -0500

    Moc e inyección de dependencias

---
+++ GbsoDev.TechTest.Library.MSTest/TestInitializer.cs
@@ -0,0 +1,34 @@
+﻿using GbsoDev.TechTest.Library.Bll;
+using GbsoDev.TechTest.Library.Wal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GbsoDev.TechTest.Library.MSTest
+{
+	[TestClass]
+	public static class TestInitializer
+	{
+		
+
+		/// <summary>
+		/// Proveedor de servicios por inyección
+		/// </summary>
+		public readonly static CustomServiceCollection ServiceProvider = new CustomServiceCollection();
+
+		/// <summary>
+		/// Inicializa las inyecciónes requeridas por los servicios
+		/// </summary>
+		[AssemblyInitialize]
+		public static void AssemblyInitializer(TestContext context)
+		{
+			var services = new ServiceCollection()
+				.AddBllValidationRulesLayer()
+				.AddMapperConfiguration();
+			ServiceProvider
+				.CustomAddRange(services)
+				.AddLoggers<BaseController>()
+				.AddLoggers<BaseService>();
+		}
+
+		
+	}
+}
